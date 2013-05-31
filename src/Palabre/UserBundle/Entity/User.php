86223983daa405a8161e89e4c3ecd4de45666b8b
Benoit de Jacobet <benijaco@gmail.com>
2013-05-31 14:32:26 +0200

    Ajout prenom et nom dans user

--- src/Palabre/UserBundle/Entity/User.php
+++ src/Palabre/UserBundle/Entity/User.php
@@ -17,6 +17,16 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $lastName;
     
     /**
      * @var \Doctrine\Common\Collections\ArrayCollection  
@@ -25,4 +35,41 @@ class User extends BaseUser
      * @ORM\JoinTable(name="palabre_project_user")
      */
     protected $projects;
+
+    public function __construct(){
+        parent::__construct();
+
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
