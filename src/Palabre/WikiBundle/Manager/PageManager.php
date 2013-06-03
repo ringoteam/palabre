9b4a44ec665a410ce2860d6c5d49a8923a2109dc
root <root@ubuntu-VirtualBox.(none)>
2013-06-03 21:06:26 +0200

    WikiBundle Suite

---
+++ src/Palabre/WikiBundle/Manager/PageManager.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Palabre\WikiBundle\Manager;
+
+use Palabre\WikiBundle\Model\Page;
+use Palabre\WikiBundle\Manager\PageManagerInterface;
+
+/**
+* Class PageManager
+*
+*/
+class PageManager implements PageManagerInterface
+{
+
+}
